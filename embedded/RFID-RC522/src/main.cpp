65cc3c1a35c048d71492e2d13dd3be4bf95b33cf
Grittie <grittielars@gmail.com>
2024-09-07 20:51:28 +0200

    added more comments and changed beep duration on startup

--- embedded/RFID-RC522/src/main.cpp
+++ embedded/RFID-RC522/src/main.cpp
@@ -10,12 +10,12 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 void setup() {
   Serial.begin(115200);
-  SPI.begin(36, 37, 35, 10);  // SCK = 36, MISO = 37, MOSI = 35, SS = 10
-  mfrc522.PCD_Init();         // Initialize MFRC522
-  pinMode(LED_PIN, OUTPUT);   // Set LED pin as output
-  digitalWrite(LED_PIN, LOW); // Ensure LED is off initially
-  pinMode(BUZZER_PIN, OUTPUT);
-  tone(BUZZER_PIN, 1000, 2000);
+  SPI.begin(36, 37, 35, 10);    // SCK = 36, MISO = 37, MOSI = 35, SS = 10
+  mfrc522.PCD_Init();           // Initialize MFRC522
+  pinMode(LED_PIN, OUTPUT);     // Set LED pin as output
+  digitalWrite(LED_PIN, LOW);   // Ensure LED is off initially
+  pinMode(BUZZER_PIN, OUTPUT);  // Buzzer pin as output
+  tone(BUZZER_PIN, 1000, 500); // Play a tone to indicate the system is ready
 }
 
 bool cardScanned = false;  // Flag to check if card has been scanned
@@ -47,13 +47,17 @@ void loop() {
   // Card is detected and scanned
   Serial.println("Card detected");
   Serial.print("UID: ");
-  String content = "";
+
+  String content = "";  // Initialize an empty string to store the UID
   for (byte i = 0; i < mfrc522.uid.size; i++) {
+    // Print each byte of the UID to the serial monitor in hexadecimal format
     Serial.print(mfrc522.uid.uidByte[i], HEX);
+    // Concatenate the UID byte to the content string, formatting it properly
     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
     content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
-  Serial.println();  // Add a newline for readability
+  
+  Serial.println();
   content.toUpperCase();
 
   // Set the flags to indicate that the card has been scanned and is present
