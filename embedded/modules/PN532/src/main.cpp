d46f87b8115164fd2c2693e5fdd6887d424e52fc
@gritla <lars.grit@hva.nl>
2024-10-18 22:19:19 +0200

    new pn532 implementation

--- embedded/modules/PN532/src/main.cpp
+++ embedded/modules/PN532/src/main.cpp
@@ -1,57 +1,84 @@
-#include <Wire.h>
-#include <Adafruit_PN532.h>
-#include <LiquidCrystal_I2C.h> // Library for the I2C LCD
+#include <PN532_HSU.h>
+#include <PN532.h>
 
-// I2C Pins for ESP32
-#define SDA_PIN 21
-#define SCL_PIN 22
+PN532_HSU pn532hsu(Serial); // Using Serial2 for PN532 communication
+PN532 nfc(pn532hsu);
+String tagId = "None", dispTag = "None";
+byte nuidPICC[4];
 
-// Create an instance for PN532 and LCD
-Adafruit_PN532 nfc(SDA_PIN, SCL_PIN);
-LiquidCrystal_I2C lcd(0x27, 16, 2); // Change 0x27 to your LCD's I2C address
+void readNFC();
+String tagToString(byte id[4]);
 
-void setup(void) {
-  Serial.begin(115200);
-  Serial.println("Looking for PN532...");
-
+void setup(void)
+{
+  Serial.begin(115200, SERIAL_8N1, 3, 1); // Initialize Serial2 with your desired pins (e.g., RX=16, TX=17)
+  Serial.println("Hello Maker!");
   nfc.begin();
-  lcd.init(); // Initialize the LCD
-  lcd.backlight(); // Turn on the LCD backlight
-
+  
   uint32_t versiondata = nfc.getFirmwareVersion();
-  if (!versiondata) {
-    Serial.println("Didn't find PN532 board");
-    while (1); // Halt if no PN532 is found
+  if (!versiondata)
+  {
+    delay(1000);
+    Serial.println("Didn't Find PN53x Module");
+    while (1); // Halt
   }
 
-  nfc.SAMConfig(); // Initialize PN532 to detect cards
-  Serial.println("Waiting for an NFC card...");
-  lcd.setCursor(0, 0);
-  lcd.print("Waiting for card...");
+  // Got valid data, print it out!
+  Serial.print("Found chip PN5");
+  Serial.println((versiondata >> 24) & 0xFF, HEX);
+  Serial.print("Firmware ver. ");
+  Serial.print((versiondata >> 16) & 0xFF, DEC);
+  Serial.print('.'); 
+  Serial.println((versiondata >> 8) & 0xFF, DEC);
+
+  // Configure board to read RFID tags
+  nfc.SAMConfig();
 }
 
-void loop(void) {
-  uint8_t success;
-  uint8_t uid[7]; // Buffer to store the UID
-  uint8_t uidLength;
+void loop()
+{
+  readNFC();
+}
 
-  // Try to read a card
-  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
-
-  if (success) {
-    // Display the UID in Serial Monitor
-    Serial.print("Card detected! UID: ");
-    lcd.clear(); // Clear the LCD before showing the UID
-    lcd.setCursor(0, 0);
-    lcd.print("Card UID:");
-    lcd.setCursor(0, 1);
-    
-    for (uint8_t i = 0; i < uidLength; i++) {
-      Serial.print(uid[i], HEX); // Print UID in HEX format
-      lcd.print(uid[i], HEX);    // Show UID on LCD
-      lcd.print(" ");
+void readNFC()
+{
+  boolean success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
+  if (success)
+  {
+    Serial.print("UID Length: ");
+    Serial.print(uidLength, DEC);
+    Serial.println(" bytes");
+    Serial.print("UID Value: ");
+    for (uint8_t i = 0; i < uidLength; i++)
+    {
+      nuidPICC[i] = uid[i];
+      Serial.print(" "); Serial.print(uid[i], DEC);
     }
     Serial.println();
-    delay(3000); // Wait before looking for another card
+    tagId = tagToString(nuidPICC);
+    dispTag = tagId;
+    Serial.print(F("tagId is : "));
+    Serial.println(tagId);
+    Serial.println("");
+    delay(1000);  // 1 second halt
   }
+  else
+  {
+    // PN532 probably timed out waiting for a card
+    Serial.println("Timed out! Waiting for a card...");
+  }
+}
+
+String tagToString(byte id[4])
+{
+  String tagId = "";
+  for (byte i = 0; i < 4; i++)
+  {
+    if (i < 3) tagId += String(id[i]) + ".";
+    else tagId += String(id[i]);
+  }
+  return tagId;
 }
